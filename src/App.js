5a6d31f2aa99d8cd7ad9bb0f73c19baf690d690b
zamanmayed1 <zamanmayed02@gmail.com>
2022-04-25 22:29:43 +0600

    Project Set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Errorpage from "./components/Pages/Errorpage";
+import Home from "./components/Pages/Home";
+import Login from "./components/Pages/Login";
+import Signup from "./components/Pages/Signup";
+import Footer from "./components/Shared/Footer";
+import Header from "./components/Shared/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="*" element={<Errorpage></Errorpage>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Pages/Errorpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Errorpage = () => {
+    return (
+        <div>
+            <h2> 404 Page Not Found</h2>
+        </div>
+    );
+};
+
+export default Errorpage;
